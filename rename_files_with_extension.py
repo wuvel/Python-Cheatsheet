2984cfad9b5652f975d5fe4e645807be3afcf405
Muhammad Fikri Ashari <41507857+wuvel@users.noreply.github.com>
2021-02-21 22:49:50 +0700

    Create rename_files_with_extension.py

---
+++ rename_files_with_extension.py
@@ -0,0 +1,19 @@
+# Question 1
+# Given a list of filenames, we want to rename all the files with extension hpp to the extension h. To do this, we would like to generate a new list called newfilenames, consisting of the new filenames. Fill in the blanks in the code using any of the methods you’ve learned thus far, like a for loop or a list comprehension.
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+temp = ""
+newfilenames = []
+for file in filenames:
+    ind = file.index(".")
+    if(file[ind:] == ".hpp"):
+        temp = file[:ind] + ".h"
+        newfilenames.append(temp)
+    else:
+        newfilenames.append(file)
+
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
