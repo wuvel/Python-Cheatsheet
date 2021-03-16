9e59751b3b999a789908a36242b8f43b6d314d24
Muhammad Fikri Ashari <41507857+wuvel@users.noreply.github.com>
2021-03-17 00:08:30 +0700

    Update emails.py

--- course_6/week_4/emails.py
+++ course_6/week_4/emails.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
+ 
+import email.message
+import mimetypes
+import os.path
+import smtplib
+ 
+def generate_email(sender, recipient, subject, body, attachment_path):
+  """Creates an email with an attachement."""
+  # Basic Email formatting
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
+ 
+  # Process the attachment and add it to the email
+  attachment_filename = os.path.basename(attachment_path)
+  mime_type, _ = mimetypes.guess_type(attachment_path)
+  mime_type, mime_subtype = mime_type.split('/', 1)
+ 
+  with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+                           maintype=mime_type,
+                           subtype=mime_subtype,
+                           filename=attachment_filename)
+ 
+  return message
+ 
+def send_email(message):
+  """Sends the message to the configured SMTP server."""
+  mail_server = smtplib.SMTP('localhost')
+  mail_server.send_message(message)
+  mail_server.quit()
 
-import os
-from datetime import date
-import reports
-import emails
+def generate_error_report(sender, recipient, subject, body):
+  """Creates an email without an attachement."""
+  # Basic Email formatting
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
 
-fruits_dict = {}
-fruits_array = list()
-
-os.chdir("/home/student-01-58646e03997a/supplier-data/descriptions")
-for filename in os.listdir("."):
-  with open(filename) as f:
-    fruits_dict['name'] = f.readline().strip()
-    fruits_dict['weight'] = f.readline().strip()
-    f.readline().strip()
-    fruits_array.append("name: {}\nweight: {}\n".format(fruits_dict['name'], fruits_dict['weight']))
-
-# Get Today Date
-today = date.today()
-formatted_date = today.strftime("%B %d, %Y")
-title_date = "Processed Update on {}".format(formatted_date)
-
-if __name__ == "__main__":
-  body_pdf = ""
-  body_email = ""
-
-  # Formatted summary
-  for s in fruits_array:
-    body_pdf += s + "<br/><br/>"
-    body_email += s + "\n\n"
-
-  reports.generate_report("/tmp/processed.pdf", title_date, body_pdf)
-
-  sender = "automation@example.com"
-  receiver = "{}@example.com".format(os.environ.get('USER'))
-  subject = "Upload Completed - Online Fruit Store"
-  body = body_email
-
-  message = emails.generate_email(sender, receiver, subject, body, "/tmp/processed.pdf")
-  emails.send_email(message)
-    
+  return message
