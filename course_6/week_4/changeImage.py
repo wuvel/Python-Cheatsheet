e6ac2c7197bd84f4998458f245630bf07a86097a
Muhammad Fikri Ashari <41507857+wuvel@users.noreply.github.com>
2021-03-17 00:06:13 +0700

    Update changeImage.py

--- course_6/week_4/changeImage.py
+++ course_6/week_4/changeImage.py
@@ -3,7 +3,7 @@
 import os
 from PIL import Image
 
-path = "/home/student-02-c6bc602fd706/supplier-data/images/"
+path = "/home/student-01-58646e03997a/supplier-data/images/"
 os.chdir(path)
 for filename in os.listdir("."):
     if not filename.endswith(".tiff"):
